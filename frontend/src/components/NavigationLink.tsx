27527eb409733cb5263470a6383b70bff44027f2
Aidan Heller <asfheller@gmail.com>
2019-04-27 15:06:27 -0700

    Add navigation link component

---
+++ frontend/src/components/NavigationLink.tsx
@@ -0,0 +1,25 @@
+import React, { createElement } from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import { Link } from 'react-router-dom';
+
+const NavigationItem = (props: NavigationItemProps) => {
+  const { icon, text, linkTo } = props;
+  return (
+    <Link to={linkTo} style={{ textDecoration: 'none', color: 'inherit' }}>
+      <ListItem button>
+        <ListItemIcon>{createElement(icon)}</ListItemIcon>
+        <ListItemText primary={text} />
+      </ListItem>
+    </Link>
+  );
+};
+
+export interface NavigationItemProps {
+  icon: React.ComponentType;
+  text: string;
+  linkTo: string;
+}
+
+export default NavigationItem;
