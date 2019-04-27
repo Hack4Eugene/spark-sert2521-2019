0a9c7c9eff9f3d3693b50e1c0d2d7e70845be29b
Andrew Dassonville <dassonville.andrew@gmail.com>
2019-04-27 09:46:01 -0700

    Add payment processing (#30)

--- backend/src/main/kotlin/me/andrewda/Server.kt
+++ backend/src/main/kotlin/me/andrewda/Server.kt
@@ -11,7 +11,6 @@ import io.ktor.features.StatusPages
 import io.ktor.gson.gson
 import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
-import io.ktor.response.respondRedirect
 import io.ktor.routing.get
 import io.ktor.routing.route
 import io.ktor.routing.routing
@@ -19,7 +18,6 @@ import me.andrewda.authentication.JwtConfig
 import me.andrewda.constants.Routes
 import me.andrewda.controllers.UserPrincipal
 import me.andrewda.handlers.*
-import me.andrewda.payment.PayPal
 import me.andrewda.utils.Database
 import me.andrewda.utils.ExceptionWithStatus
 import me.andrewda.utils.Status
@@ -27,8 +25,6 @@ import org.slf4j.event.Level
 
 fun Application.main() {
     Database.init()
-
-    PayPal.createPayment(1.53) ?: return
 
     install(Authentication) {
         jwt {
@@ -75,16 +71,9 @@ fun Application.main() {
             person()
             item()
             request()
+            payment()
 
             test()
-
-            get("/payments/return") {
-                val paymentId = call.request.queryParameters["paymentId"] ?: ""
-                val payerId = call.request.queryParameters["PayerID"] ?: ""
-                PayPal.executePayment(paymentId, payerId)
-
-                call.respondRedirect("/payment/success")
-            }
 
             // Route any unspecified API requests to 404
             get("{...}") {
---
+++ backend/src/main/kotlin/me/andrewda/handlers/PaymentHandler.kt
@@ -0,0 +1,92 @@
+package me.andrewda.handlers
+
+import io.ktor.application.call
+import io.ktor.auth.authenticate
+import io.ktor.request.receiveOrNull
+import io.ktor.response.respondRedirect
+import io.ktor.routing.Route
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.route
+import me.andrewda.controllers.PaymentController
+import me.andrewda.controllers.PersonController
+import me.andrewda.controllers.RequestController
+import me.andrewda.models.NewPayment
+import me.andrewda.payment.PayPal
+import me.andrewda.utils.*
+
+fun Route.payment() {
+    route("/payments") {
+        route("/people") {
+            post("/{slug}") {
+                val slug = call.parameters["slug"] ?: throw NotFound()
+                val newPayment = call.receiveOrNull<NewPayment>() ?: throw MissingFields()
+                val user = call.getUser()
+
+                val person = PersonController.findBySlug(slug) ?: throw NotFound()
+
+                val amount = newPayment.amount ?: throw InvalidAmount()
+
+                if (amount <= 0) throw InvalidAmount()
+
+                if (newPayment.isValid) {
+                    val payment = PayPal.createPayment(amount) ?: throw MissingFields()
+                    val link = payment.links.find { it.rel == "approval_url" }?.href ?: throw InternalServerError()
+
+                    try {
+                        PaymentController.create(newPayment, payment.id, user, person)
+                    } catch (exception: Exception) {
+                        throw PaymentFailure()
+                    }
+                    call.respond(mapOf("link" to link))
+                } else {
+                    throw MissingFields()
+                }
+            }
+        }
+
+        route("/requests") {
+            post("/{id}") {
+                val id = call.parameters["id"]?.toIntOrNull() ?: throw NotFound()
+                val newPayment = call.receiveOrNull<NewPayment>() ?: throw MissingFields()
+                val user = call.getUser()
+
+                val request = RequestController.findById(id) ?: throw NotFound()
+
+                val amount = newPayment.amount ?: throw InvalidAmount()
+
+                if (amount <= 0) throw InvalidAmount()
+
+                if (newPayment.isValid) {
+                    val payment = PayPal.createPayment(amount) ?: throw MissingFields()
+                    val link = payment.links.find { it.rel == "approval_url" }?.href ?: throw InternalServerError()
+
+                    try {
+                        PaymentController.create(newPayment, payment.id, user, request)
+                    } catch (exception: Exception) {
+                        throw PaymentFailure()
+                    }
+
+                    call.respond(mapOf("link" to link))
+                } else {
+                    throw MissingFields()
+                }
+            }
+        }
+
+        get("/return") {
+            val paymentId = call.request.queryParameters["paymentId"] ?: ""
+            val payerId = call.request.queryParameters["PayerID"] ?: ""
+
+            try {
+                PayPal.executePayment(paymentId, payerId)
+            } catch (exception: Exception) {
+                throw PaymentFailure()
+            }
+
+            PaymentController.completePayment(paymentId)
+
+            call.respondRedirect("/payment/success")
+        }
+    }
+}
