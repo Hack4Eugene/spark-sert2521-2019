890ba8254bb11d4824b0a443efcdb5a3269b1975
Aidan Heller <asfheller@gmail.com>
2019-04-27 15:22:24 -0700

    Implicit return

--- frontend/src/components/NavigationLink.tsx
+++ frontend/src/components/NavigationLink.tsx
@@ -4,16 +4,14 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import { Link } from 'react-router-dom';
 
-const NavigationItem = ({ icon, text, linkTo }: NavigationItemProps) => {
-  return (
-    <Link to={linkTo} style={{ textDecoration: 'none', color: 'inherit' }}>
-      <ListItem button>
-        <ListItemIcon>{createElement(icon)}</ListItemIcon>
-        <ListItemText primary={text} />
-      </ListItem>
-    </Link>
-  );
-};
+const NavigationItem = ({ icon, text, linkTo }: NavigationItemProps) => (
+  <Link to={linkTo} style={{ textDecoration: 'none', color: 'inherit' }}>
+    <ListItem button>
+      <ListItemIcon>{createElement(icon)}</ListItemIcon>
+      <ListItemText primary={text} />
+    </ListItem>
+  </Link>
+);
 
 export interface NavigationItemProps {
   icon: React.ComponentType;
