084df7c3ef1b5f6d9575fdb3de46c2bb046f3cf5
Aidan Heller <asfheller@gmail.com>
2019-04-27 10:49:50 -0700

    Make main app component

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,26 +1,14 @@
-import React, { Component } from 'react';
-import '../App.css';
+import React, { createElement } from 'react';
+import { Route, Switch } from "react-router";
+import { CssBaseline } from "@material-ui/core";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+    <>
+      <CssBaseline />
+      <Switch>
+        <Route path="/" component={() => null} />
+      </Switch>
+    </>
+);
 
 export default App;
