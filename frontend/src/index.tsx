45094e0aecedf19aa5452a597b7a0d59dbddaf15
William <whou125@gmail.com>
2019-04-26 21:06:52 -0700

    Restructure frontend directories

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Link, Route} from "react-router-dom";
-import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 function App() {
