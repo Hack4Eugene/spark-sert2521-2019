3cd68962fc4b20672f9bb427abe8605ecf456183
Aidan Heller <asfheller@gmail.com>
2019-04-27 01:25:00 -0700

    Setup redux (#29)
    * Setup redux
    
    * Fix inconsistent styles

---
+++ frontend/src/state/reducer.ts
@@ -0,0 +1,34 @@
+import { ActionType } from 'typesafe-actions';
+import * as actions from './actions';
+import { Person, Request, User } from './index';
+import {combineReducers} from 'redux';
+import { ActionTypes } from './actions';
+
+const user = (state: User | null = null, action: ActionType<typeof actions>) => {
+    switch (action.type) {
+        case ActionTypes.UPDATE_USER:
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+const people = (state: Array<Person> = [], action: ActionType<typeof actions>) => {
+    switch (action.type) {
+        case ActionTypes.UPDATE_PEOPLE:
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+const requests = (state: Array<Request> = [], action: ActionType<typeof actions>) => {
+    switch (action.type) {
+        case ActionTypes.UPDATE_REQUESTS:
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+export const reducer = combineReducers({ user, people, requests });
