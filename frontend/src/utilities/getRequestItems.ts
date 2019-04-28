7975cf71a4fe3c57f59d943b4c465ab66ca43aeb
Andrew Dassonville <dassonville.andrew@gmail.com>
2019-04-27 22:00:19 -0700

    Add thank you page (#67)

--- frontend/src/utilities/getRequestItems.ts
+++ frontend/src/utilities/getRequestItems.ts
@@ -10,7 +10,7 @@ export default async () => {
   const items = await axios('http://localhost:8080/api/items');
   //const mappedItems: Options =
   return items.data.response.map(({ name, price, id }: Request) => {
-    price = Number(((price * 100) / 100).toFixed(2));
-    return { label: `${name} - $${price}`, value: id };
+    const formattedPrice = ((price * 100) / 100).toFixed(2);
+    return { label: `${name} - $${formattedPrice}`, value: id };
   });
 };
